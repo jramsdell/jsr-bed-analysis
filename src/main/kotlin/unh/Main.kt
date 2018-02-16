af08b94c2de8e70612e9a040d9d5ca459405ed75
Vadavim <vadavim@gmail.com>
2018-02-16 09:47:53 -0500

    Initial commit

--- src/main/kotlin/unh/Main.kt
+++ src/main/kotlin/unh/Main.kt
@@ -1,5 +1,12 @@
 package unh
 
+import com.xenomachina.argparser.ShowHelpException
+
 fun main(args: Array<String>) {
-    runBedGraphParser(args)
+    try {
+        runBedGraphParser(args)
+    } catch (e: ShowHelpException) {
+        System.out.println("QWEQWE")
+        e.printAndExit()
+    }
 }
