349aeece45d9a90b25365800f801f2e936b679ef
Vadavim <vadavim@gmail.com>
2018-02-16 09:14:39 -0500

    Initial commit

--- src/main/kotlin/unh/BedGraphParser.kt
+++ src/main/kotlin/unh/BedGraphParser.kt
@@ -2,38 +2,43 @@
 package unh
 
 import com.xenomachina.argparser.ArgParser
-import com.xenomachina.argparser.SystemExitException
 import com.xenomachina.argparser.default
+import com.xenomachina.text.clear
 import java.io.File
 import java.lang.Math.abs
+
+
 
 class MyParser(parser: ArgParser) {
     val bedFile by parser.storing("-b", "--bedFile",
             help = "BedGraph file containing genome coverage.")
     val interval by parser.storing("-n", "--interval",
             help = "Size of sliding window (in bp) over genome").default { "50" }
+    val fasta by parser.storing("-f", "--fasta",
+            help = "Fasta map for reference.").default { "" }
 }
 
 class Interval(var location: String, var start: Int, var stop: Int, var coverage: Double) {
     var intervals = ArrayList<Interval>()
+    var times = 1
     operator fun plus(interval: Interval): Interval {
         if (interval.location != location || abs(interval.start - stop) > 5) {
             interval.intervals = this.intervals.let { it += this; it }
             return interval
         }
         stop = interval.stop
-        coverage = (interval.coverage + coverage) / 2
+//        coverage = (interval.coverage + coverage) / 2
+        coverage += interval.coverage
+        times += 1
         return this
     }
 }
 
-var mycount = 0
 
 
 fun parseFile(filename: String): Interval =
         File(filename).bufferedReader().lineSequence()
                 .drop(1)
-                .onEach { mycount += 1 }
                 .map { line ->
                     line.split("\t").run {
                         Interval(this[0], this[1].toInt(), this[2].toInt(), this[3].toDouble())
@@ -42,24 +47,71 @@ fun parseFile(filename: String): Interval =
                 .filter { interval -> interval.coverage > 2 }
                 .reduce(Interval::plus)
 
+fun readFasta(filename: String): HashMap<String, String> {
+    val fastaMap = HashMap<String, String>()
+    var header = ""
+    val builder = StringBuilder()
+
+    File(filename).readLines().forEach { line ->
+        if (line.startsWith(">")) {
+            if (header != "") {
+                fastaMap[header] = builder.toString()
+                builder.clear()
+            }
+
+            header = line
+        } else {
+            builder.append(line.trimEnd())
+        }
+    }
+    return fastaMap
+}
+
+fun extractSequences(p: MyParser, fasta: HashMap<String, String>,
+                     results: Map<Int, List<Interval>>) {
+    val pattern = "([ATCG]GG)".toRegex()
+
+    val pamCountsByBin: List<Pair<Int, Double>> =
+            results.map { (binSize,count) ->
+                // For each interval, extract corresponding sequence and count PAM Sites
+                val pamAverage = count.map { interval ->
+                    fasta[interval.location]!!
+                            .substring(interval.start, interval.stop)
+                            .run { pattern.findAll(this).count() }
+                }.sum().toDouble() / count.size // Averages counts in the bin
+
+                binSize to pamAverage
+            }
+
+    pamCountsByBin.forEach { (k,v) ->
+        println("$k: $v")
+    }
+}
+
 
 fun main(args: Array<String>) {
-    val myArgs = arrayOf("--bedFile", "Sample_ZS1_human.bedGraph")
+    val myArgs = arrayOf("--bedFile", "Sample_ZS1_human.bedGraph", "--interval", "50",
+            "--fasta", "hg38.fa")
     val parser = ArgParser(myArgs)
     val p = MyParser(parser)
     val intervals = parseFile(p.bedFile)
-    val results = intervals.intervals.groupBy { (it.stop - it.start) / 50 }
+    val results = intervals.intervals.groupBy { (it.stop - it.start) / p.interval.toInt() }
 
-    println("Histogram of Fragment Size (bin size: 50)")
+    println("Histogram of Fragment Size (bin size: ${p.interval.toInt()})")
     results.entries
             .sortedBy { it.key }
-            .forEach { (k,v) -> println("${k.toInt() * 50}: ${v.size}") }
+            .forEach { (k,v) -> println("${k * p.interval.toInt()}: ${v.size}") }
 
     println("\nAverage Coverage of Binned Fragments")
-    results .map { (k, v) -> k to v.sumByDouble { it.coverage } / v.size }
+    results .map { (k, v) -> k to v.sumByDouble { it.coverage / it.times } / v.size }
             .sortedBy { it.first }
-            .forEach { (k, v) -> println("${k.toInt() * 50}: $v") }
+            .forEach { (k, v) -> println("${k * p.interval.toInt()}: $v") }
 
+    if (p.fasta != "") {
+        val fasta = readFasta(p.fasta)
+        extractSequences(p, fasta, results)
+    }
 
 
 }
+
